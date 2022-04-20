bee56a32abaf97b29d1f10b8e688e4f8aded5bb2
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2022-04-20 16:43:51 -0400

    found the best 4 starting words

--- Source Files/find_best_words.cpp
+++ Source Files/find_best_words.cpp
@@ -38,31 +38,31 @@ char alphaPlaceToChar(int place) {
 //matching probability*100
 const std::unordered_map<char, std::vector<int>> matchingProbability = {
     {'a', {109, 234, 237, 126,  49}},
-
+    {'b', {133,  12,  44,  18,   8}},
     {'c', {153,  31,  43, 117,  24}},
     {'d', { 86,  15,  58,  53,  91}},
     {'e', { 55, 187, 136, 245, 327}},
-
-
+    {'f', {105,   6,  19,  27,  20}},
+    {'g', { 89,   9,  52,  59,  32}},
     {'h', { 53, 111,   7,  22, 107}},
     {'i', { 26, 156, 205, 122,   8}},
-
-
+    {'j', { 15,   2,   2,   2,   0}},
+    {'k', { 15,   8,   9,  42,  87}},
     {'l', { 68, 155,  86, 125, 120}},
-
+    {'m', { 82,  29,  47,  52,  32}},
     {'n', { 29,  67, 107, 140, 100}},
     {'o', { 32, 215, 188, 102,  45}},
     {'p', {109,  47,  45,  39,  43}},
-
+    {'q', { 18,   4,   1,   0,   0}},
     {'r', { 81, 206, 126, 117, 163}},
     {'s', {282,  12,  62, 132,  28}},
     {'t', {115,  59,  86, 107, 195}},
     {'u', { 25, 143, 127,  63,   1}},
-
-
-
+    {'v', { 33,  12,  38,  35,   0}},
+    {'w', { 64,  34,  20,  19,  13}},
+    {'x', {  0,  11,   9,   2,   6}},
     {'y', {  5,  18,  22,   2, 281}},
-
+    {'z', {  2,   2,   8,  15,   3}}
 };
 
 int score(std::string word) {
@@ -73,12 +73,8 @@ int score(std::string word) {
     return currentScore;
 }
 
-void saveWords(std::string word1, std::string word2, std::string word3) {
-    std::vector<std::string> goodSet;
-    goodSet.push_back(word1);
-    goodSet.push_back(word2);
-    goodSet.push_back(word3);
-    bestWords.push_back(goodSet);
+void saveWords(std::vector<std::string> strings) {
+    bestWords.push_back(strings);
 }
 
 int main() {
@@ -87,7 +83,7 @@ int main() {
         'e', 'a', 'r', 'o', 't',
         'l', 'i', 's', 'n', 'u',
         'c', 'y', 'h', 'd', 'p',
-        // 'g', 'm', 'b', 'f', 'k',
+        'g', 'm', 'b', 'f', 'k',
         // 'w', 'v', 'x', 'z', 'q',
         // 'j'
     };
@@ -128,13 +124,14 @@ int main() {
     std::cout << "testing combinations ";
     int highestScore = 0;
     std::vector<char> skipChars;
-    skipChars.resize(12);
+    skipChars.resize(17);
     for (int aFirst = 0; aFirst < words.size()-2; aFirst++) {
         //I hate this but it works since this is how the words vector is already sorted
         skipChars[0] = usingLetters[aFirst];
         for (int aSecond = 0; aSecond < words[aFirst].size(); aSecond++) {
             skipChars[1] = alphaPlaceToChar(aSecond);
             for (int ina = 0; ina < words[aFirst][aSecond].size(); ina++) {
+                // std::cout << "A: " << words[aFirst][aSecond][ina] << '\n';
                 std::vector<char> unusedLettersA = usingLetters;
                 if (!removeUsedLetters(unusedLettersA, words[aFirst][aSecond][ina])) //If the word has a letter that's not in unusedLetters
                     continue;
@@ -152,6 +149,7 @@ int main() {
                             continue;
                         skipChars[6] = alphaPlaceToChar(bSecond);
                         for (int inb = 0; inb < words[bFirst][bSecond].size(); inb++) {
+                            // std::cout << "B: " << words[bFirst][bSecond][inb] << '\n';
                             std::vector<char> unusedLettersB = unusedLettersA;
                             if (!removeUsedLetters(unusedLettersB, words[bFirst][bSecond][inb]))
                                 continue;
@@ -168,17 +166,38 @@ int main() {
                                         continue;
                                     skipChars[11] = alphaPlaceToChar(cSecond);
                                     for (int inc = 0; inc < words[cFirst][cSecond].size(); inc++) {
+                                        // std::cout << "C: " << words[cFirst][cSecond][inc] << '\n';
                                         std::vector<char> unusedLettersC = unusedLettersB;
-                                        if (!testUsedLetters(unusedLettersC, words[cFirst][cSecond][inc]))
+                                        if (!removeUsedLetters(unusedLettersC, words[cFirst][cSecond][inc]))
                                             continue;
+                                        skipChars[12] = words[cFirst][cSecond][inc][2];
+                                        skipChars[13] = words[cFirst][cSecond][inc][3];
+                                        skipChars[14] = words[cFirst][cSecond][inc][4];
                                         int scoreC = scoreB + score(words[cFirst][cSecond][inc]);
-                                        if (scoreC > highestScore) {
-                                            highestScore = scoreC;
-                                            bestWords.clear();
-                                            //Save these words
-                                            saveWords(words[aFirst][aSecond][ina], words[bFirst][bSecond][inb], words[cFirst][cSecond][inc]);
-                                        } else if (scoreC == highestScore)
-                                            saveWords(words[aFirst][aSecond][ina], words[bFirst][bSecond][inb], words[cFirst][cSecond][inc]);
+                                        for (int dFirst = cFirst+1; dFirst < words.size(); dFirst++) {
+                                            if (std::find(skipChars.begin(), skipChars.begin() + 15, usingLetters[dFirst]) != skipChars.begin() + 15)
+                                                continue;
+                                            skipChars[15] = usingLetters[dFirst];
+                                            for (int dSecond = 0; dSecond < words[dFirst].size(); dSecond++) {
+                                                if (std::find(skipChars.begin(), skipChars.begin() + 16, alphaPlaceToChar(dSecond)) != skipChars.begin() + 16)
+                                                    continue;
+                                                skipChars[16] = alphaPlaceToChar(dSecond);
+                                                for (int ind = 0; ind < words[dFirst][dSecond].size(); ind++) {
+                                                    // std::cout << "D: " << words[dFirst][dSecond][ind] << '\n';
+                                                    std::vector<char> unusedLettersD = unusedLettersC;
+                                                    if (!testUsedLetters(unusedLettersD, words[dFirst][dSecond][ind]))
+                                                        continue;
+                                                    int scoreD = scoreC + score(words[dFirst][dSecond][ind]);
+                                                    if (scoreD > highestScore) {
+                                                        highestScore = scoreD;
+                                                        bestWords.clear();
+                                                        //Save these words
+                                                        saveWords({words[aFirst][aSecond][ina], words[bFirst][bSecond][inb], words[cFirst][cSecond][inc], words[dFirst][dSecond][ind]});
+                                                    } else if (scoreD == highestScore)
+                                                        saveWords({words[aFirst][aSecond][ina], words[bFirst][bSecond][inb], words[cFirst][cSecond][inc], words[dFirst][dSecond][ind]});
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -202,5 +221,5 @@ int main() {
 }
 
 //Best 2 words: 
-//Best 3 words: soily, prude, chant / shady, crine, poult / shaly, coude, print / shaly, count, pride / shaly, crude, point / shule, coady, print / shunt, coaly, pride / shale, crudy, point
-//Best 4 words: 
+//Best 3 words: soily prude chant / shady crine poult / shaly coude print / shaly count pride / shaly crude point / shule coady print / shunt coaly pride / shale crudy point
+//Best 4 words: shack podgy brume flint / shame podgy brunt flick / shuck podgy brame flint / shuck print bodge flamy / shunt podge blimy frack / shunt podge brick flamy / shunt podgy brame flick / shunt podgy brick flame / shunt prick bodge flamy / shunt primy bodge flack
---
+++ Unused Files/pick_best_words.cpp
@@ -0,0 +1,25 @@
+// #include <vector>
+// #include <string>
+
+// std::vector<std::vector<std::string>> bestWords = {
+//     {"shack", "podgy", "brume", "flint"},
+//     {"shame", "podgy", "brunt", "flick"},
+//     {"shuck", "podgy", "brame", "flint"},
+//     {"shuck", "print", "bodge", "flamy"},
+//     {"shunt", "podge", "blimy", "frack"},
+//     {"shunt", "podge", "brick", "flamy"},
+//     {"shunt", "podgy", "brame", "flick"},
+//     {"shunt", "podgy", "brick", "flame"},
+//     {"shunt", "prick", "bodge", "flamy"},
+//     {"shunt", "primy", "bodge", "flack"}
+// };
+
+// /**
+//  * @brief choose list of words that has the word with the lowest score
+//  * 
+//  * @return int 
+//  */
+// int main() {
+
+//     return 0;
+// }
